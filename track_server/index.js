ea49c5e95a163e35cd572966344ac7d38cf654a6
Kunal Khallar <khallark295@gmail.com>
2025-02-22 19:02:42 +0530

    modified:   track_server/index.js

--- track_server/index.js
+++ track_server/index.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: '10mb' }))
 const PORT = process.env.PORT || 3000
 
 app.use(cors({
-    origin: 'localhost:5173',
+    origin: 'track-app-pi.vercel.app',
     credentials: true
 }))
 
