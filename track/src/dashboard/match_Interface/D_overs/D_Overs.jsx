f80b51faaffc58bcbdd4f67efd131dd8b778eaf5
Kunal Khallar <khallark295@gmail.com>
2025-03-04 10:41:56 +0530

    modified:   track/src/dashboard/match_Interface/D_overs/D_Overs.jsx 	modified:   track/src/matches/match/Overs.jsx

--- track/src/dashboard/match_Interface/D_overs/D_Overs.jsx
+++ track/src/dashboard/match_Interface/D_overs/D_Overs.jsx
@@ -18,7 +18,7 @@ export default function D_Overs() {
                     {overs_1.map((over, index) => (
                         <div key={nanoid()} className={`py-2 flex flex-col`}>
                             <div className={`flex 700:gap-x-1`}>
-                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${theme ? 'text-[#3b3b3b]' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
+                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${theme ? 'text-[#3b3b3b]  text-nowrap' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
                                 <div className={`flex flex-wrap gap-[.2rem] 700:gap-x-1`}>
                                     {over.slice(1).map((val) => (
                                         <h1 key={nanoid()} className={`flex items-center ${theme ? 'text-[#3b3b3b]' :'text-[#CCCCCC]'} ${theme ? 'bg-white border border-[#cccccc] hover:border-[#005bf8]' : 'border border-[#37373d] hover:border-[#0078d4] bg-[#37373d]'} font-mono rounded-md leading-none py-1 px-2 700:px-3 text-[.7rem] 700:text-[.9rem] cursor-pointer ${theme ? 'hover:brightness-[.95] active:brightness-[.8]' : 'hover:brightness-[1.2] active:brightness-[1]'}`}>{val}</h1>
@@ -37,7 +37,7 @@ export default function D_Overs() {
                     {overs_2.map((over, index) => (
                         <div key={nanoid()} className={`py-2 flex flex-col`}>
                             <div className={`flex 700:gap-x-1`}>
-                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${theme ? 'text-[#3b3b3b]' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
+                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${theme ? 'text-[#3b3b3b] text-nowrap' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
                                 <div className={`flex flex-wrap gap-[.2rem] 700:gap-x-1`}>
                                     {over.slice(1).map((val) => (
                                         <h1 key={nanoid()} className={`flex items-center ${theme ? 'text-[#3b3b3b]' :'text-[#CCCCCC]'} ${theme ? 'bg-white border border-[#cccccc] hover:border-[#005bf8]' : 'border border-[#37373d] hover:border-[#0078d4] bg-[#37373d]'} font-mono rounded-md leading-none py-1 px-2 700:px-3 text-[.7rem] 700:text-[.9rem] cursor-pointer ${theme ? 'hover:brightness-[.95] active:brightness-[.8]' : 'hover:brightness-[1.2] active:brightness-[1]'}`}>{val}</h1>
--- track/src/matches/match/Overs.jsx
+++ track/src/matches/match/Overs.jsx
@@ -1,7 +1,6 @@
 import { nanoid } from '@reduxjs/toolkit'
 import React, { useEffect, useState } from 'react'
 import { useOutletContext } from 'react-router-dom'
-import { Line } from 'react-chartjs-2'
 import Graph from './Graph'
 
 export default function Overs() {
@@ -18,7 +17,7 @@ export default function Overs() {
     }, [])
     return (
         <div className={`p-3 w-full h-full overflow-x-auto overflow-auto ${true ? 'text-[#3b3b3b]' :'text-[#CCCCCC]'} ${true ? '' : 'custom-scrollbar'}`}>
-            <div className='transition scale-75 500:scale-[1] flex justify-center'>
+            <div className='transition scale-[.8] 500:scale-[1] flex justify-center'>
                 {data ? <Graph size={Math.min(300, Math.max(200, windowWidth / 2 - 100))} match={data}/> : ''}
             </div>
             {[
@@ -29,7 +28,7 @@ export default function Overs() {
                     {overs_1.map((over, index) => (
                         <div key={nanoid()} className={`py-2 flex flex-col`}>
                             <div className={`flex 700:gap-x-1`}>
-                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${true ? 'text-[#3b3b3b]' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
+                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${true ? 'text-[#3b3b3b] text-nowrap' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
                                 <div className={`flex flex-wrap gap-[.2rem] 700:gap-x-1`}>
                                     {over.slice(1).map((val) => (
                                         <h1 key={nanoid()} className={`flex items-center e${true ? 'text-[#3b3b3b]' :'text-[#CCCCCC]'} ${true ? 'bg-white border border-[#cccccc] hover:border-[#005bf8]' : 'border border-[#37373d] hover:border-[#0078d4] bg-[#37373d]'} font-mono rounded-md leading-none py-1 px-2 700:px-3 text-[.7rem] 700:text-[.9rem] cursor-pointer ${true ? 'hover:brightness-[.95] active:brightness-[.8]' : 'hover:brightness-[1.2] active:brightness-[1]'}`}>{val}</h1>
@@ -48,7 +47,7 @@ export default function Overs() {
                     {overs_2.map((over, index) => (
                         <div key={nanoid()} className={`py-2 flex flex-col`}>
                             <div className={`flex 700:gap-x-1`}>
-                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${true ? 'text-[#3b3b3b]' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
+                                <h1 className={`font-robotoSans pr-3 text-[.9rem] 700:text-[1rem] ${true ? 'text-[#3b3b3b] text-nowrap' : 'text-[#797979] text-nowrap'}`}>[ {index + 1} ] &nbsp; <span className='font-robotoSans font-[400] underline underline-offset-4 text-nowrap'>{over[0]}</span></h1>
                                 <div className={`flex flex-wrap gap-[.2rem] 700:gap-x-1`}>
                                     {over.slice(1).map((val) => (
                                         <h1 key={nanoid()} className={`flex items-center ${true ? 'text-[#3b3b3b]' :'text-[#CCCCCC]'} ${true ? 'bg-white border border-[#cccccc] hover:border-[#005bf8]' : 'border border-[#37373d] hover:border-[#0078d4] bg-[#37373d]'} font-mono rounded-md leading-none py-1 px-2 700:px-3 text-[.7rem] 700:text-[.9rem] cursor-pointer ${true ? 'hover:brightness-[.95] active:brightness-[.8]' : 'hover:brightness-[1.2] active:brightness-[1]'}`}>{val}</h1>
